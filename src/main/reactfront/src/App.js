f8d3b193c5d14267df9e15201bef8001c2d2e223
sosow0212 <sosow0212@naver.com>
2021-11-08 20:06:23 +0900

    .

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [name, setname] = useState("Lee1");
+
   return (
     <div className="App">
-      Hi React
+      <h1>H1 태그</h1>
+
+      {name}
+
+      <h2>{name} -- h2안에 변수적용</h2>
     </div>
   );
 }
