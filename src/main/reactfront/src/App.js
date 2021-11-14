b4133e8c8eb51b591d649b0b09e5d38180c30c9c
sosow0212 <sosow0212@naver.com>
2021-11-14 17:16:46 +0900

    .

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -2,14 +2,30 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
-  let [num, setNum] = useState(1);
+  const [num, setNum] = useState(0);
+
+  const [numList, setNumList] = useState([]);
+
+  function numRecording() {
+    setNumList([...numList, num]); 
+    // ...은 배열에 기존 데이터를 의미하고, num을 저장한다는 의미이다.
+    // 즉 기존의 배열에 새로운 데이터인 num을 추가하는 것이다.
+    
+    setNum(0);
+  }
 
   return (
     <div className="App">
-      <div style={{marginTop:"10%", marginBottom:"3%", fontSize:"30px"}}>React + Spring boot 프론트엔드와 백엔드 동시 개발해보기</div>
-      <div style={{color : "red", fontSize : "30px"}}>Style 적용해보기</div>
-      <div>{num}</div>
-      <button onClick={() => {setNum(num = num + 1)}}>버튼</button>
+      <div>현재 숫자 : {num}</div>
+      <button onClick={() => {setNum(num+1)}}>숫자 증가</button>
+      <button onClick={() => {setNum(num-1)}}>숫자 감소</button>
+      <button onClick={numRecording}>숫자 기록</button>
+      <h1>저장된 숫자</h1>
+      <ul>
+        {numList.map((num) => (
+          <li>{num}</li>
+        ))}
+      </ul>
     </div>
   );
 }
