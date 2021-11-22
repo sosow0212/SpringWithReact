849c10cbf10cb2a71dfb355aca962c07febe0e59
sosow0212 <sosow0212@naver.com>
2021-11-22 18:58:24 +0900

    .

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -1,33 +1,44 @@
 import "./App.css";
 import React, { useState } from "react";
 
-function App() {
+// 함수의 첫글자는 대문자로!
+const RecordForm = ({ numList, setNumList }) => {
   const [num, setNum] = useState(0);
 
-  const [numList, setNumList] = useState([]);
-
-  function numRecording() {
-    setNumList([...numList, num]); 
-    // ...은 배열에 기존 데이터를 의미하고, num을 저장한다는 의미이다.
-    // 즉 기존의 배열에 새로운 데이터인 num을 추가하는 것이다.
-    
-    setNum(0);
-  }
-
   return (
-    <div className="App">
+    <div>
       <div>현재 숫자 : {num}</div>
-      <button onClick={() => {setNum(num+1)}}>숫자 증가</button>
-      <button onClick={() => {setNum(num-1)}}>숫자 감소</button>
-      <button onClick={numRecording}>숫자 기록</button>
-      <h1>저장된 숫자</h1>
-      <ul>
-        {numList.map((num) => (
-          <li>{num}</li>
-        ))}
-      </ul>
+      <button onClick={() => setNum(num + 1)}>숫자 증가</button>
+      <button onClick={() => setNum(num - 1)}>숫자 감소</button>
+      <button onClick={() => setNum(0)}>숫자 초기화</button>
+      <hr />
+      <button onClick={() => setNumList([...numList, num])}>숫자 기록</button>
+      <button onClick={() => setNumList([])}>기록 초기화</button>
     </div>
   );
-}
+};
+
+const RecordList = ({numList}) => {
+  return (
+    <div>
+      <h2>기록된 숫자</h2>
+      {numList.length > 0 ? <div>기록 있음</div> : 
+
+
+      <div>기록 없음</div>}
+    </div>
+  );
+};
+
+const App = () => {
+  const [numList, setNumList] = useState([]);
+
+  return (
+    <div style={{ margin: "0 auto", width: "800px", textAlign: "center" }}>
+      <RecordForm numList={numList} setNumList={setNumList} />
+      <RecordList numList={numList} />
+    </div>
+  );
+};
 
 export default App;
